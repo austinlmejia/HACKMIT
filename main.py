7b1d078df897325d31133d0be13856d004c3eebc
Claudia <CMH@Claudias-MacBook-Air.local>
2017-09-17 11:59:03 -0400

    IT WORKSSSSS

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ import time
 
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
-
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template = env.get_template('main.html')
+        template = jinja_environment.get_template('main.html')
         self.response.write(template.render())
 
 class Profile(webapp2.RequestHandler):
